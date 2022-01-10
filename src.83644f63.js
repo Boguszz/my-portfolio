b626a4b8ff2681c83d21a57410eaf5fdb91748e6
Boguszz <87972200+Boguszz@users.noreply.github.com>
2022-01-10 15:52:49 +0000

    Deploying to gh-pages from @ Boguszz/my-portfolio@69b8b558c06112f5566f01fef402f69f75edd8a7 🚀

--- src.83644f63.js
+++ src.83644f63.js
@@ -1,6 +1,10 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
-},{"./../images/hero/hero1@1x.png":[["hero1@1x.776d49e3.png","Q46m"],"Q46m"]}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/my-portfolio/src.1ab37094.js.map
+},{"./../images/hero/hero1@1x.png":[["hero1@1x.776d49e3.png","Q46m"],"Q46m"]}],"eYiS":[function(require,module,exports) {
+const e=document.querySelectorAll('a[href^="#"]');for(let t of e)t.addEventListener("click",function(e){e.preventDefault();const o=t.getAttribute("href");document.querySelector(o).scrollIntoView({behavior:"smooth",block:"start"})});
+},{}],"yruE":[function(require,module,exports) {
+const e=document.querySelector(".buton-up");function t(){const t=window.pageYOffset,n=document.documentElement.clientHeight;t>n&&e.classList.add("is-show"),t<n&&e.classList.remove("is-show")}function n(){window.pageYOffset>0&&(window.scrollBy(0,-50),setTimeout(n,0))}window.addEventListener("scroll",t),e.addEventListener("click",n);
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss"),require("./js/smoth-scroll/index"),require("./js/scroll-up/index");
+},{"./sass/main.scss":"clu1","./js/smoth-scroll/index":"eYiS","./js/scroll-up/index":"yruE"}]},{},["Focm"], null)
+//# sourceMappingURL=/my-portfolio/src.83644f63.js.map
